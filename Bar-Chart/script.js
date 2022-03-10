bd3807642915010668da517665a1b13100ecb7c2
kadimakeith <keithkadima@gmail.com>
2022-03-10 15:37:04 +0300

    x-axis scale refactor

--- Bar-Chart/script.js
+++ Bar-Chart/script.js
@@ -13,12 +13,14 @@ document.addEventListener('DOMContentLoaded',function(){
             
             let html = "";
             console.log(gdp_data)
+
+            
             
         
             
             //d3 visualisation
-            const min_date = d3.min(gdp_data, (d) => d[0])
-            const max_date = d3.max(gdp_data, (d) => d[0])
+            const min_date = d3.min(gdp_data, (d) => parseInt(d[0].substring(0,4)))
+            const max_date = d3.max(gdp_data, (d) => parseInt(d[0].substring(0,4)))
             console.log(min_date)
             console.log(max_date)
             const min_gdp_val = d3.min(gdp_data, (d) => d[1]);
@@ -29,8 +31,9 @@ document.addEventListener('DOMContentLoaded',function(){
             const yscale = d3.scaleLinear()
                         .domain([0, max_gdp_val])
                         .range([h - padding, padding]);
+            console.log(yscale(300))            
             const xscale = d3.scaleLinear()
-                        .domain([0 , max_date])
+                        .domain([min_date , max_date])
                         .range([padding , w - padding]);
             console.log( xscale(5))                        
             const xaxis = d3.axisBottom(xscale)
@@ -45,20 +48,21 @@ document.addEventListener('DOMContentLoaded',function(){
                         .data(gdp_data)
                         .enter()
                         .append("rect")
-                        .attr("x", function(d, i){ return i*6})
+                        .attr("x", function(d, i){ return i*(6)})
                         .attr("y", (d , i) => h - d[1])
-                        .attr("width", 5)
+                        .attr("width", 3)
                         .attr("height", function( d , i) {return d[1]}) 
-                        .attr("fill", "#B4D8D8")
                         .attr("class","bar")
                         .append("title")
-                        .text((d) => d[1])
+                        .text((d) => d[0])
 
                     svg.append("g")
                         .attr("transform", "translate(0," + (h - padding) + ")")
+                        .attr("id", "x-axis")
                         .call(xaxis);
                     svg.append("g")
                         .attr("transform", "translate(" + padding + ",0)")
+                        .attr("id", "y-axis")
                         .call(yaxis)
 
 
