9b9f42290af75a507e80ae349e7948e6db5e7038
kadimakeith <keithkadima@gmail.com>
2022-03-11 20:25:19 +0300

    code refactor

--- Bar-Chart/script.js
+++ Bar-Chart/script.js
@@ -1,4 +1,5 @@
 const padding = 50;
+const barWidth = 3;
 const w = 1000;
 const h = 1000;
 
@@ -9,33 +10,33 @@ document.addEventListener('DOMContentLoaded',function(){
         fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
         .then(response => response.json())
         .then(data => {
-            let gdp_data = data.data
-            
-            let html = "";
-            console.log(gdp_data)
+            let gdp_data = data.data.map((item) => item[1])
+            let years = data.data.map((item) => new Date(item[0]));
+            let scaledGDP = [];
 
             
-            
-        
-            
+            let html = "";
+
             //d3 visualisation
-            const min_date = d3.min(gdp_data, (d) => parseInt(d[0].substring(0,4)))
-            const max_date = d3.max(gdp_data, (d) => parseInt(d[0].substring(0,4)))
-            console.log(min_date)
-            console.log(max_date)
-            const min_gdp_val = d3.min(gdp_data, (d) => d[1]);
-            const max_gdp_val = d3.max(gdp_data, (d) => d[1])
-            console.log(min_gdp_val)
-            console.log(max_gdp_val)
+            const min_date = d3.min(years)
+            const max_date = d3.max(years)
+            
+            const min_gdp_val = d3.min(gdp_data);
+            const max_gdp_val = d3.max(gdp_data)
+            
 
             const yscale = d3.scaleLinear()
                         .domain([0, max_gdp_val])
                         .range([h - padding, padding]);
-            console.log(yscale(300))            
-            const xscale = d3.scaleLinear()
+
+            scaledGDP = gdp_data.map((item) => yscale(item))
+                       
+           
+            const xscale = d3.scaleTime()
                         .domain([min_date , max_date])
                         .range([padding , w - padding]);
-            console.log( xscale(5))                        
+
+                                    
             const xaxis = d3.axisBottom(xscale)
             const yaxis = d3.axisLeft(yscale)            
             const svg = d3.select("section")
@@ -45,41 +46,30 @@ document.addEventListener('DOMContentLoaded',function(){
 
                     // to work on visualization later
                     svg.selectAll("rect")
-                        .data(gdp_data)
+                        .data(scaledGDP)
                         .enter()
                         .append("rect")
-                        .attr("x", function(d, i){ return i*(6)})
-                        .attr("y", (d , i) => h - d[1])
-                        .attr("width", 3)
-                        .attr("height", function( d , i) {return d[1]}) 
+                        .attr("x", function(d, i){ return xscale(years[i])})
+                        .attr("y", (d , i) => h - (padding + d))
+                        .attr("width", barWidth)
+                        .attr("height", function( d , i) {return d})
+                        .attr("data-date", (d , i) => data.data[i][0])
+                        .attr("data-gdp", (d , i) => data.data[i][1])
                         .attr("class","bar")
+                        .attr("index", (d , i) => i)
                         .append("title")
-                        .text((d) => d[0])
+                        .text((d) => d)
 
                     svg.append("g")
                         .attr("transform", "translate(0," + (h - padding) + ")")
                         .attr("id", "x-axis")
                         .call(xaxis);
+
                     svg.append("g")
                         .attr("transform", "translate(" + padding + ",0)")
                         .attr("id", "y-axis")
                         .call(yaxis)
-
-
-            gdp_data.forEach(function(val) {
-
-                const keys = Object.keys(val);
-                html += "<div class = 'data'>";
-
-                keys.forEach(function(key) {
-                    
-                    html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
-                });
-
-                html += "</div><br>";
-            });
-
-            document.getElementById('message').innerHTML = html;
+        
         })
       
     };
